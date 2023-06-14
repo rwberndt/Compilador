05d9898a6c35bfab432e3e7ff4f351b8583d7e09
rwberndt <rwberndt@furb.br>
2023-06-14 17:41:39 -0300

    metodos 17 a 20

--- interfaceCompilador/src/main/java/ArquivosGals/Semantico.java
+++ interfaceCompilador/src/main/java/ArquivosGals/Semantico.java
@@ -7,7 +7,7 @@ public class Semantico implements Constants
 {
     private String operador; 	// tipo string
     private ArrayList<String> codigo;  	// tipo lista de string
-    Stack<Integer> pilha_tipos; // tipo pilha int64 - 1 float64 - 2 String - 3 bool-4
+    Stack<Integer> pilha_tipos; // tipo pilha int64 - 1 float64 - 2 String - 3 bool - 4
 
 
     public void executeAction(int action, Token token)	throws SemanticError
@@ -15,9 +15,44 @@ public class Semantico implements Constants
         switch (action) {
 
             case 1 : metodo_acao01();
-            break;
+                break;
             case 5:metodo_acao05(token);
-            break;
+                break;
+            case 6: metodo_acao06(token);
+                break;
+            case 2 : metodo_acao02();
+                break;
+            case 3: metodo_acao03();
+                break;
+            case 4: metodo_acao04();
+                break;
+            case 7: metodo_acao07();
+                break;
+            case 8: metodo_acao08();
+                break;
+            case 10: metodo_acao10();
+                break;
+            case 11 : metodo_acao11();
+                break;
+            case 12: metodo_acao12();
+                break;
+            case 13: metodo_acao13();
+                break;
+            case 14: metodo_acao14();
+                break;
+            case 15: metodo_acao15();
+                break;
+            case 16: metodo_acao16();
+                break;
+            case 17: metodo_acao17();
+                break;
+            case 18: metodo_acao18();
+                break;
+            case 19: metodo_acao19();
+                break;
+            case 20: metodo_acao20();
+                break;
+
         }
             System.out.println("Ação #"+action+", Token: "+token);
     }
@@ -158,5 +193,69 @@ public class Semantico implements Constants
         codigo.add("xor");
     }
 
+    public void metodo_acao17(){
+        codigo.add("ldstr " + "\"\\n\"");
+        codigo.add("call void [mscorlib]System.Console::Write(string)");
+    }
+    public void metodo_acao18() throws SemanticError {
+        Integer tipo1 = pilha_tipos.pop();
+        Integer tipo2 = pilha_tipos.pop();
+        if(tipo1 != tipo2)
+            throw new SemanticError("os tipos não são compatíveis");
+        pilha_tipos.push(4);
+        codigo.add("and");
+    }
+    public void metodo_acao19() throws SemanticError{
+        Integer tipo1 = pilha_tipos.pop();
+        Integer tipo2 = pilha_tipos.pop();
+        if(tipo1 != tipo2)
+            throw new SemanticError("os tipos não são compatíveis");
 
-}
+        pilha_tipos.push(4);
+        codigo.add("or");
+    }
+    public void metodo_acao20()throws SemanticError {
+        Integer tipo1 = pilha_tipos.pop();
+        Integer tipo2 = pilha_tipos.pop();
+        if (!tipo1.equals("int64") || !tipo2.equals("int64")) {
+            throw new SemanticError("tipo(s) incompatível(is) em expressão aritmética");
+        }
+
+        if (tipo1.equals(1)||tipo2.equals(1)){
+            pilha_tipos.push(1);
+        }
+        else {
+            pilha_tipos.push(2);
+        }
+
+        codigo.add("conv.i8");
+        codigo.add("stloc divisor");
+
+        codigo.add("conv.i8");
+        codigo.add("stloc dividendo");
+
+        codigo.add("ldloc dividendo");
+        codigo.add("conv.r8");
+
+        codigo.add("ldloc dividendo");
+        codigo.add("conv.r8");
+
+        codigo.add("ldloc divisor");
+        codigo.add("conv.r8");
+
+        codigo.add("div");
+        codigo.add("conv.i8");
+        codigo.add("conv.r8");
+
+        codigo.add("ldloc divisor");
+        codigo.add("conv.r8");
+
+        codigo.add("mul");
+        codigo.add("sub");
+    }
+
+    }
+
+
+
+
